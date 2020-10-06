d7284f06a0db0ca4e6b25b59edd978307d56104f
Becher <mbelkhenchir@gmail.com>
2020-10-06 17:41:20 +0200

    Command System

---
+++ Assets/Script/Twich/CommandLog.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Twitch
+{
+    public class CommandLog : MonoBehaviour
+    {
+        private TextMeshProUGUI log;
+        private float timer = 10;
+
+        public void Init(string text)
+        {
+            log = GetComponent<TextMeshProUGUI>();
+            log.text = text;
+        }
+
+        public void Update()
+        {
+            timer -= Time.deltaTime;
+            if (timer < 1)
+            {
+                Color c = log.color;
+                c.a = timer;
+                log.color = c;
+            }
+
+            if (timer <= 0) Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Script/Twich/Robot.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Twitch
+{
+    public class Robot : MonoBehaviour
+    {
+        public static Robot tesla;
+        public static Robot edison;
+
+        public string name;
+        public int nbPaw = 4;
+        public int maxBuffer = 10;
+        public float maxActionTime = 10;
+        public Stack<int> bufferLeft;
+        public Stack<int> bufferRight;
+
+        private bool active;
+        
+        void Awake()
+        {
+            if (name.ToLower() == "tesla") tesla = this;
+            else edison = this;
+        }
+
+        void Update()
+        {
+            
+        }
+
+        public void EndAction()
+        {
+            // TODO END ACTION OR NEXT ACTION
+        }
+        
+        public bool Command(int paw)
+        {
+            if (paw >= nbPaw) return false;
+
+            if (paw < nbPaw / 2)
+            {
+                if (active)
+                {
+                    bufferLeft.Push(paw);
+                    if (bufferLeft.Count > maxBuffer) bufferLeft.Pop();
+                }
+                else
+                {
+                    active = true;
+                    //TODO LAUNCH ACTION
+                }
+            }
+            else
+            {
+                if (active)
+                {
+                    bufferRight.Push(paw);
+                    if (bufferRight.Count > maxBuffer) bufferRight.Pop();
+                }
+
+                else
+                {
+                    active = true;
+                    //TODO LAUNCH ACTION
+                }
+            }
+            return true;
+        }
+    }
+}
