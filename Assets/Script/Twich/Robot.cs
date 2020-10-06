231ceddcd24899330e0ca2811fad28c59a0f6ef8
Becher <mbelkhenchir@gmail.com>
2020-10-06 23:04:22 +0200

    Event Manager

--- Assets/Script/Twich/Robot.cs
+++ Assets/Script/Twich/Robot.cs
@@ -10,28 +10,53 @@ namespace Twitch
         public static Robot edison;
 
         public string name;
+        public RobotType type;
         public int nbPaw = 4;
         public int maxBuffer = 10;
         public float maxActionTime = 10;
-        public Stack<int> bufferLeft;
-        public Stack<int> bufferRight;
+        [Header("Event")] 
+        [SerializeField] private EventCommand eventCommand;
 
-        private bool active;
+        public int activeLeft;
+        [HideInInspector] public Stack<int> bufferLeft;
+        public int activeRight;
+        [HideInInspector] public Stack<int> bufferRight;
+
         
-        void Awake()
+        private void Awake()
         {
-            if (name.ToLower() == "tesla") tesla = this;
-            else edison = this;
+            if (name.ToLower() == "tesla")
+            {
+                type = RobotType.tesla;
+                tesla = this;
+            }
+            else
+            {
+                type = RobotType.edison;
+                edison = this;
+            }
+
+            eventCommand.onEndAction += EndAction;
+            eventCommand.onBlockPaw += BlockPaw;
         }
 
-        void Update()
+        private void Update()
         {
             
         }
 
-        public void EndAction()
+        private void EndAction(RobotType _type, int paw)
         {
-            // TODO END ACTION OR NEXT ACTION
+            if (_type != type) return;
+            if (paw < nbPaw / 2) NextActionLeft();
+            else NextActionRight();
+        }
+
+        private void BlockPaw(RobotType _type, int paw)
+        {
+            if (_type != type) return;
+            Debug.Log("BlockPaw");
+            EndAction(_type, paw);
         }
         
         public bool Command(int paw)
@@ -40,20 +65,21 @@ namespace Twitch
 
             if (paw < nbPaw / 2)
             {
-                if (active)
+                if (activeLeft > 0)
                 {
                     bufferLeft.Push(paw);
                     if (bufferLeft.Count > maxBuffer) bufferLeft.Pop();
                 }
                 else
                 {
-                    active = true;
-                    //TODO LAUNCH ACTION
+                    activeLeft = paw;
+                    eventCommand.CallActivatePaw(type, activeLeft);
                 }
+                eventCommand.CallSetCurrentSpeed(type, activeLeft,maxActionTime/bufferLeft.Count);
             }
             else
             {
-                if (active)
+                if (activeRight > 0)
                 {
                     bufferRight.Push(paw);
                     if (bufferRight.Count > maxBuffer) bufferRight.Pop();
@@ -61,11 +87,35 @@ namespace Twitch
 
                 else
                 {
-                    active = true;
-                    //TODO LAUNCH ACTION
+                    activeRight = paw;
+                    eventCommand.CallActivatePaw(type, activeRight);
+                    
                 }
+                eventCommand.CallSetCurrentSpeed(type, activeRight,maxActionTime/bufferRight.Count);
             }
             return true;
+        }
+
+        
+        private void NextActionLeft()
+        {
+            if (bufferLeft.Count > 0)
+            {
+                activeLeft = bufferLeft.Pop();
+                eventCommand.CallActivatePaw(type, activeLeft);
+            }
+            else activeLeft = -1;
+            eventCommand.CallSetCurrentSpeed(type, activeLeft, maxActionTime/bufferLeft.Count);
+        }
+        private void NextActionRight()
+        {
+            if (bufferRight.Count > 0)
+            {
+                activeRight = bufferRight.Pop();
+                eventCommand.CallActivatePaw(type, activeRight);
+            }
+            else activeRight = -1;
+            eventCommand.CallSetCurrentSpeed(type, activeRight, maxActionTime/bufferRight.Count);
         }
     }
 }
