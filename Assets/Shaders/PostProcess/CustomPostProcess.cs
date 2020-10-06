786573923a16f2d77a993a76bd9bcd3e4bcac8c4
SamuelBUSSON <samuel.busson@hotmail.fr>
2020-10-06 16:55:08 +0200

    ✨ Adding toon shader

---
+++ Assets/Shaders/PostProcess/CustomPostProcess.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[Serializable]
+[PostProcess(typeof(ComicsRenderer), PostProcessEvent.BeforeStack, "Custom/Comics")]
+public class CustomPostProcess : PostProcessEffectSettings
+{
+    [Tooltip("Tilling intensity.")]
+    public Vector2Parameter tilling = new Vector2Parameter();
+    
+    [Tooltip("Circle texture")]
+    public TextureParameter circleTexture = new TextureParameter();
+}
+
+public sealed class ComicsRenderer : PostProcessEffectRenderer<CustomPostProcess>
+{
+    public override void Render(PostProcessRenderContext context)
+    {
+        var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/Comics"));
+        sheet.properties.SetVector("_Tilling", settings.tilling);
+        sheet.properties.SetTexture("_CircleTexture", settings.circleTexture);
+        
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+    }
+}
