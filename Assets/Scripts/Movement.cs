89b5e1f0d437b404ed947d781c093eda8f25fb78
Guilhem PECH <guilhempech@gmail.com>
2020-10-07 14:34:52 +0200

    :construction: Add spider v1

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,37 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using DG.Tweening;
-using DG.Tweening.Core;
-using DG.Tweening.Plugins.Core.PathCore;
-using DG.Tweening.Plugins.Options;
+﻿using DG.Tweening;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 { // Update is called once per frame
 
-    public Transform endEffector;
-    private bool _test = true;
+    public Transform topLeftLeg;
+    public Transform topRightLeg;
+    public Transform bottomLeftLeg;
+    public Transform bottomRightLeg;
 
-    void Update()
+    private bool _movementInProgress;
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Move();
-        }
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+            MoveLeg(bottomLeftLeg);
+        else if (Input.GetKeyDown(KeyCode.Keypad2))
+            MoveLeg(bottomRightLeg);
+        else if (Input.GetKeyDown(KeyCode.Keypad4))
+            MoveLeg(topLeftLeg);
+        else if (Input.GetKeyDown(KeyCode.Keypad5)) 
+            MoveLeg(topRightLeg);
+
+        
+        // Replace correctly the spider body
+        float z = transform.position.z;
+        Vector3 pos = (topLeftLeg.position + topRightLeg.position + bottomLeftLeg.position +
+                             bottomRightLeg.position) / 4;
+        pos.z = z;
+        transform.position = pos;
     }
     
    
 
-    private void Move()
+    private void MoveLeg(Transform leg)
     {
-        if (!_test) return;
-        
-        Vector3 pos = endEffector.localPosition;
+        if (_movementInProgress) return;
+        Vector3 pos = leg.position;
         Vector3[] path = {pos, pos - Vector3.forward, pos + Vector3.up};
-
-        endEffector.DOLocalPath(path, 8f, PathType.CatmullRom)
-            .OnStart(() => _test = false)
-            .OnComplete(() => _test = true);
-        
+        leg.DOPath(path, 0.5f, PathType.CatmullRom)
+            .OnStart(() => _movementInProgress = true)    
+            .OnComplete(() => _movementInProgress = false);
     }
 }
