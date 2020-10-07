a847afe7324b8f6f15d408e6fb714abd2eb23c23
Guilhem PECH <guilhempech@gmail.com>
2020-10-07 17:27:34 +0200

    🩹 Fix merge

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using DG.Tweening;
 using DG.Tweening.Core;
 using DG.Tweening.Plugins.Core.PathCore;
@@ -8,27 +9,55 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 { // Update is called once per frame
 
-    public Transform topLeftLeg;
-    public Transform topRightLeg;
-    public Transform bottomLeftLeg;
-    public Transform bottomRightLeg;
-    public float animSpeedScale = 1f;
-    
+    public Transform topLeftLeg; //0
+    public Transform topRightLeg;//1
+    public Transform bottomLeftLeg;//2
+    public Transform bottomRightLeg;//3
+    public float animSpeedScale = 1f;//Soleil
+    public EventCommand command;
     
     private bool _movementInProgress;
     private bool _midMovement;
-    private TweenerCore<Vector3, Path, PathOptions> _tween;
-    
+    private TweenerCore<Vector3, Path, PathOptions> _tweenLeft;
+    private TweenerCore<Vector3, Path, PathOptions> _tweenRight;
+    public RobotType team;
+    private void Awake()
+    {
+        command.onActivatePaw += OnActivePaw;
+        command.onSetCurrentSpeed += OnChangeTimescale;
+    }
+
+    private void OnChangeTimescale(RobotType type, int paw, float speed)
+    {
+        if (team != type) return;// Arnaud Tsamer
+        if (paw % 2 == 0 )
+        {
+            
+            _tweenLeft.timeScale = speed;
+        }
+        else
+        {
+            _tweenRight.timeScale = speed;
+        }
+    }
+
+    private void OnActivePaw(RobotType type, int paw)
+    {
+        if (team != type) return;// Arnaud Tsamer
+        
+        MoveLeg(paw, (paw % 2 == 0) ? _tweenLeft : _tweenRight);
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Keypad1))
-            MoveLeg(bottomLeftLeg);
-        else if (Input.GetKeyDown(KeyCode.Keypad2))
-            MoveLeg(bottomRightLeg);
-        else if (Input.GetKeyDown(KeyCode.Keypad4))
-            MoveLeg(topLeftLeg);
-        else if (Input.GetKeyDown(KeyCode.Keypad5)) 
-            MoveLeg(topRightLeg);
+        // if (Input.GetKeyDown(KeyCode.Keypad1))
+        //     MoveLeg(bottomLeftLeg);
+        // else if (Input.GetKeyDown(KeyCode.Keypad2))
+        //     MoveLeg(bottomRightLeg);
+        // else if (Input.GetKeyDown(KeyCode.Keypad4))
+        //     MoveLeg(topLeftLeg);
+        // else if (Input.GetKeyDown(KeyCode.Keypad5)) 
+        //     MoveLeg(topRightLeg);
 
         
         // Replace correctly the spider body
@@ -76,35 +105,59 @@ public class Movement : MonoBehaviour
     }
 
     private bool _willFall;
-    private void MoveLeg(Transform leg)
+    private void MoveLeg(int paw, TweenerCore<Vector3, Path, PathOptions>  tween)
     {
-        if (_movementInProgress || CheckIfLegsCollide(leg)) return;
-         _willFall = (Math.Abs(leg.position.y - GetHighestLeg().position.y) < 0.2f && !CheckDistanceLegs());
+        Transform leg;
+        switch (paw)
+        {
+            case 0:
+                leg = topLeftLeg;
+                break;
+            case 1:
+                leg = topRightLeg;
+                break;
+            case 2:
+                leg = bottomLeftLeg;
+                break;
+            default:
+                leg = bottomRightLeg;
+                break;
+
+        }
+        
+        if (_movementInProgress || CheckIfLegsCollide(leg))
+        {
+            command.CallBlockPaw(team,paw);
+            return;
+        }
+
+        _willFall = (Math.Abs(leg.position.y - GetHighestLeg().position.y) < 0.2f && !CheckDistanceLegs());
         _midMovement = false;
         Vector3 pos = leg.position;
         Vector3[] path = {pos, pos - Vector3.forward, pos + Vector3.up};
-        _tween = leg.DOPath(path, 1f, PathType.CatmullRom)
+        tween = leg.DOPath(path, 1f, PathType.CatmullRom)
             .OnStart(OnStartMovement)
-            .OnUpdate(OnMovementUpdate)
-            .OnComplete(OnMovementCompleted);
+            .OnUpdate(() => OnMovementUpdate(tween))
+            .OnComplete(() => OnMovementCompleted(paw));
         
+        if (paw % 2 == 0) _tweenLeft = tween;
+        else _tweenRight = tween;
     }
 
-    private void FallSpider()
+    private void FallSpider(int paw)
     {
         var positionLowest = GetLowestLeg().position;
-        bottomLeftLeg.DOMoveY(positionLowest.y, 1f).SetDelay(0.1f).OnStart(() => _movementInProgress = true);
+        bottomLeftLeg.DOMoveY(positionLowest.y, 1f).SetDelay(0.1f);
         bottomRightLeg.DOMoveY(positionLowest.y, 1f).SetDelay(0.1f);
         topLeftLeg.DOMoveY(positionLowest.y + 1,1f).SetDelay(0.1f);
-        topRightLeg.DOMoveY(positionLowest.y + 1, 1f).SetDelay(0.1f).OnComplete(() => _movementInProgress = false);
+        topRightLeg.DOMoveY(positionLowest.y + 1, 1f).SetDelay(0.1f).OnComplete(() => command.CallEndAction(team,paw));
     }
 
-    private void OnMovementUpdate()
+    private void OnMovementUpdate(TweenerCore<Vector3, Path, PathOptions> tweenerCore)
     {
-        _tween.timeScale = animSpeedScale;
-        if (_tween.ElapsedPercentage() > 0.5f && !_midMovement)
+        if (tweenerCore.ElapsedPercentage() > 0.5f && !_midMovement)
         {
-            _midMovement = true;
+            _midMovement = true; //TODO PLUS TARD FDP (IL Y EN A 2 MTN)
             OnMovementMid();
         } 
     }
@@ -114,14 +167,22 @@ public class Movement : MonoBehaviour
         // TODO some sound
     }
 
-    private void OnMovementCompleted()
+    private void OnMovementCompleted(int paw)
     {
         _movementInProgress = false;
-        _tween.Kill();
-        _tween = null;
         if(_willFall)
-            FallSpider();
+            FallSpider(paw);
+        else
+        {
+            StartCoroutine(EndAction(paw));
+        }
         _willFall = false;
+    }
+
+    private IEnumerator EndAction(int paw)
+    {
+        yield return new WaitForSeconds(0.01f);
+        command.CallEndAction(team, paw);
     }
 
     private void OnStartMovement()
